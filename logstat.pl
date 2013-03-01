2e49c5deb66c5f40f4b0240f9bc949d2becc065b
Toshi Azwad <toshi0104@gmail.com>
2013-03-02 01:51:16 +0900

    1st upload

---
+++ logstat.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Getopt::Long 'GetOptions';
+use File::Tail;
+#for LTSV log format
+
+my %opts;
+
+GetOptions( \%opts, qw( find=s log=s ) );
+
+my @items = @ARGV;
+
+my $word = $opts{find} || undef;;
+my $file_name = $opts{log} || '/var/log/apache2/access.log';
+
+my $finding_word;
+if (defined($word)) {
+  $finding_word = qr/$word/;
+}else{
+	$finding_word =  undef;
+}
+
+print "log:$file_name\n";
+print "find word: $word\n" if $word;
+if (@items){
+	print "items:";
+	for (@items) {
+		print "$_ ";
+	}
+	print "\n";
+}
+
+my $file = File::Tail->new(
+		name => $file_name,
+		maxinterval => 1,
+		adjustafter	=> 10,
+);
+
+while (defined(my $line=$file->read)){
+		if ($finding_word){
+			if (($line =~ /$finding_word/ ) and (@items)){
+				filter($line);
+			}elsif ($line =~ /$finding_word/ ) {
+				print $line ."\n";
+			}else{
+				next;
+			}
+		}else{
+			if (@items) {
+				filter($line);
+			}else{
+				print $line ."\n";
+			}
+		}
+}
+
+sub filter {
+	my $line = shift;
+	my %hash = map { split( /:/, $_, 2 )} split(/\t/, $line);
+	foreach (@items){
+		print "$_:$hash{$_}\t"if exists($hash{$_})
+	}
+	print "\n";
+}
+ 
+
